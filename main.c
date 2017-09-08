4c13610ba27abfc29fc587dc496d415e9ab27fe8
flo071 <flo.hintermeier@gmail.com>
2017-09-08 08:54:44 +0200

    atmega32u4: enable SPI on master

--- main.c
+++ main.c
@@ -7,9 +7,13 @@
 
 int main(void){
     lcd_init();
-    for(;;)
-    {
-    	
+    PORTB=0xFF;
+    for(;;){
+    	void spi_init_master (void){
+    		DDRB=(1<<1)|(1<<2);								//MOSI & SCK as output
+    		SPCR=(1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);	//Enable SPI
+    		sei();											//Enable Interrupts
+		}
     }
 	return 0;
 }
