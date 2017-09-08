6fc6205bef28e89343fa63fc5e4bbcb571b5eb3b
flo071 <flo.hintermeier@gmail.com>
2017-09-08 08:49:31 +0200

    Add main.c
    -basic includes
    -lcd includes

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "lcd/LCD.c"
+
+int main(void){
+    lcd_init();
+    for(;;)
+    {
+    	
+    }
+	return 0;
+}
