3d50c91c1998c0e7457a34d9bed98016b4adaa2d
flo071 <flo.hintermeier@gmail.com>
2017-09-11 11:03:55 +0200

    keyboard: let it be readable for other stuff
    -more values
    -returns something

--- keyboard/keyboard.c
+++ keyboard/keyboard.c
@@ -3,64 +3,66 @@
 #include <util/delay.h>
 #include "keyboard.h"
 
-DDRD=0b01110000;
-PORTD=0b00001111;
 
-while(1){
-key0=0;
-key1=0;
+int keyboard(value){
+	DDRD=0b01110000;
+	PORTD=0b00001111;
 
-PORTD=0b00111111;	//row 1
-if((PIND | ZEILE1)==ZEILE1){
-	key0=key0+3;	//key 1
-}
+	while(1){
+		key0=0;
+		key1=0;
+		value=0;
 
-if((PIND | ZEILE2)==ZEILE2){
-	key0=key0+5;	//key 4
-}
+		PORTD=0b00111111;	//row 1
+		if((PIND | ZEILE1)==ZEILE1){
+			value=key0+3;	//key 1
+		}
 
-if((PIND | ZEILE3)==ZEILE3){
-	key0=key0+8;	//key 7
-}
+		if((PIND | ZEILE2)==ZEILE2){
+			value=key0+5;	//key 4
+		}
 
-if((PIND | ZEILE4)==ZEILE4){
-	key1=key1+3;	//key *
-}
+		if((PIND | ZEILE3)==ZEILE3){
+			value=key0+8;	//key 7
+		}
 
+		if((PIND | ZEILE4)==ZEILE4){
+			value=key1+3;	//key *
+		}
 
-PORTD=0b01011111;	//row 2
-if((PIND | ZEILE1)==ZEILE1){
-	key0=key0+2;	//key 2
-}
+		PORTD=0b01011111;	//row 2
+		if((PIND | ZEILE1)==ZEILE1){
+			value=key0+2;	//key 2
+		}
 
-if((PIND | ZEILE2)==ZEILE2){
-	key0=key0+4;	//key 5
-}
+		if((PIND | ZEILE2)==ZEILE2){
+			value=key0+4;	//key 5
+		}
 
-if((PIND | ZEILE3)==ZEILE3){
-	key0=key0+7;	//key 8
-}
+		if((PIND | ZEILE3)==ZEILE3){
+			value=key0+7;	//key 8
+		}
 
-if((PIND | ZEILE4)==ZEILE4){
-	key1=key1+2;	//key 0
-}
+		if((PIND | ZEILE4)==ZEILE4){
+			value=key1+2;	//key 0
+		}
 
+		PORTD=0b01101111;	//row 3
+		if((PIND | ZEILE1)==ZEILE1){
+			value=key0+1;	//key 3
+		}
 
-PORTD=0b01101111;	//row 3
-if((PIND | ZEILE1)==ZEILE1){
-	key0=key0+1;	//key 3
-}
+		if((PIND | ZEILE2)==ZEILE2){
+			value=key0+6;	//key 6
+		}
 
-if((PIND | ZEILE2)==ZEILE2){
-	key0=key0+6;	//key 6
-}
-
-if((PIND | ZEILE3)==ZEILE3){
-	key1=key1+1;	//key 9
-}
-
-if((PIND | ZEILE4)==ZEILE4){
-	key1=key1+4;	//key #
-}
-
+		if((PIND | ZEILE3)==ZEILE3){
+			value=key1+1;	//key 9
+		}
+	
+		if((PIND | ZEILE4)==ZEILE4){
+			value=key1+4;	//key #
+		}
+	}
+	return value;
 }
