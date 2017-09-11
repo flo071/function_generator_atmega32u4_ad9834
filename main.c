389f33267067e3bdac64e43a9347eebf28e07d1d
flo071 <flo.hintermeier@gmail.com>
2017-09-11 11:04:45 +0200

    main: add keyoard support
    -make it display numbers
    -display start and stop (for reasons)

--- main.c
+++ main.c
@@ -5,17 +5,36 @@
 #include <util/delay.h>
 #include "LCD/lcd.c"
 #include "SPI/spi.c"
-//#include "keyboard/keyboard.c"
+#include "keyboard/keyboard.c"
 
 int main(void){
-	PORTB=0xFF;
     lcd_init();
     lcd_setcursor(6,2);
     lcd_string("function");
-    lcd_setcursor(7,3):
+    lcd_setcursor(7,3);
     lcd_string("generator");
     for(;;){
-    	
+    	keyboard();
+        if(keyboard()<=10){
+            lcd_setcursor(0,1);
+            char out_d[30];
+            itoa(keyboard(), out_d, 30);
+            lcd_string(out_d);
+        }
+        if(keyboard()>=10){
+            if(keyboard()==10){
+                lcd_setcursor(0,1);
+                lcd_string("Stop");
+                _delay_ms(1000);
+                lcd_clear();
+            }
+            if(keyboard()==12){
+                lcd_setcursor(0,1);
+                lcd_string("Start");
+                _delay_ms(1000);
+                lcd_clear();
+            }
+        }
     }
 	return 0;
 }
