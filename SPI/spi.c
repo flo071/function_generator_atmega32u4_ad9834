d6beb35c3dec1484b1a488b6ac0c953ffc816139
flo071 <flo.hintermeier@gmail.com>
2017-09-20 09:09:04 +0200

    update

--- SPI/spi.c
+++ SPI/spi.c
@@ -2,20 +2,20 @@
 
 void spi_init(void)
 {
-  sbi(DDRB, 4);
-  sbi(DDRB, 5);
-  cbi(DDRB, 6);
-  sbi(DDRB, 7);
-  sbi(SPCR, 0);
-  sbi(SPCR, 4);
-  sbi(SPCR, 6);
+  	sbi(DDRB, 4);
+  	sbi(DDRB, 5);
+  	cbi(DDRB, 6);
+  	sbi(DDRB, 7);
+  	sbi(SPCR, 0);
+  	sbi(SPCR, 4);
+  	sbi(SPCR, 6);
 }
 
 void spi_send(uint8_t data)
 {
-  SPDR = data;
-  while(bit_is_clear(SPSR, 7))
-  {
+  	SPDR = data;
+	while(bit_is_clear(SPSR, 7))
+  	{
 
-  }
+  	}
 }
