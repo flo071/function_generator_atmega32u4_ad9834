fd4b381896f0061f8e140586089d17e68d42a010
flo071 <flo.hintermeier@gmail.com>
2018-03-22 12:22:38 +0100

    final menu layout

--- main.c
+++ main.c
@@ -5,135 +5,32 @@ Created by flo071
 #include <avr/io.h>
 #include <stdio.h>
 #include <util/delay.h>
-
-int x=0;
+#include "include/LCD/lcd.c"
 
 int main(void){
-	init();
+	lcd_init();
+    while(1){
 
-    while(x=0){
-    	M1_sig();
-		
     }
-while(x=1){
-    //do nothing
-}
 }
 
-int init(void){
-	//Disable JTAG
-	MCUCR =  (1<<JTD);
-    MCUCR =  (1<<JTD);
-    //Setup Pins for SPI
-	DDRB  |=  (1<<PB0); //SlaveSelect
-	DDRB  |=  (1<<PB1); //CLK
-	DDRB  |=  (1<<PB2); //MOSI
-	DDRB  &= ~(1<<PB3); //MISO
-	DDRB  |=  (1<<PB4); //SS Dummy
-	DDRB  |=  (1<<PB7); //SS for manual bit selection, like 16bit here
-	PORTB |=  (1<<PB7); //SS active low set high
-	//Setup SPI
-	//SPI init
-	SPCR &= ~(1<<DORD); //Data order MSB first
-	SPCR |=  (1<<MSTR); //Master mode
-	SPCR &= ~(1<<CPOL); //sclk rising edge
-	SPCR &= ~(1<<CPHA); //sclk phase 
-	SPCR |=  (1<<SPR1); //sclk prescaler /64
-	SPCR |=  (1<<SPR0); //sclk prescaler /64
-	SPSR |=  (1<<SPI2X);//sclk prescaler /64
+int lcd_init(void){
+	//LCD Init/Setup
+	lcd_init();
+	keyboardInit();
+	lcd_setcursor(1,0);
+	lcd_string("Function Generator");
+	_delay_ms(4000);
+	lcd_clear();
 }
 
-int M1_sig(){
-	x=1;
-
-	//control register write 
-	SPDR = 0b00000001;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000000;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//tell ad9834 to receive 28 bits
-	SPDR = 0b00100001;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000000;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//write to freq 0 register 1MHz
-	SPDR = 0b01000000;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b11011010;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b01011101;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000011;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);	
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//write to freq 1 register 1MHz
-	SPDR = 0b01000000;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b11011010;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b01011101;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000011;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);	
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//phase 0
-	SPDR = 0b11000000;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000000;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//phase 1
-	SPDR = 0b11000000;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000000;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-	//freq 1 output
-	SPDR = 0b00001100;
-	PORTB &= ~(1<<PB7); //SlaveSelect set low, to let ad9834 know we want to send something
-	_delay_ms(1);		//wait for SS to be set to low
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	SPDR = 0b00000000;
-	SPCR |=  (1<<SPE); 	//SPI enable (start data tranfer)
-	_delay_ms(10);		//wait for the third 8 bit to be send
-	PORTB |= (1<<PB7); 	//SlaveSelect set high
-
-    return x;
+int menu(){
+	lcd_setcursor(0,0);
+	lcd_string("Wave             [1]");
+	lcd_setcursor(0,0);
+	lcd_string("Frequency        [2]");
+	lcd_setcursor(0,0);
+	lcd_string("Amplitude        [3]");
+	lcd_setcursor(0,0);
+	lcd_string("Offset           [4]");
 }
