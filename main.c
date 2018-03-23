5cde6368a425cba77ada202147f1034e91e3a502
flo071 <flo.hintermeier@gmail.com>
2018-03-23 10:28:31 +0100

    add keypad files

--- main.c
+++ main.c
@@ -10,39 +10,8 @@ Created by flo071
 int A=0, B=0, C=0, D=0, SET=1;
 
 int main(void){
-	lcd_init();
+	keypad_init();
     while(1){
-
+    	keypad();
     }
-}
-
-int lcd_init(void){
-	//LCD Init/Setup
-	lcd_init();
-	keyboardInit();
-	lcd_setcursor(1,0);
-	lcd_string("Function Generator");
-	_delay_ms(4000);
-	lcd_clear();
-}
-
-int menu(){
-	lcd_setcursor(0,0);
-	lcd_string("Wave:            [A]");
-	lcd_setcursor(0,1);
-	lcd_string("Frequency:       [B]");
-	lcd_setcursor(0,2);
-	lcd_string("Amplitude:       [C]");
-	lcd_setcursor(0,3);
-	lcd_string("Offset:          [D]");
-	if((A|B|C|D)&SET==1){
-		lcd_setcursor(0,0);
-		lcd_string("sine");
-		lcd_setcursor(0,1);
-		lcd_string("1kHz");
-		lcd_setcursor(0,2);
-		lcd_string("5Vpp");
-		lcd_setcursor(0,3);
-		lcd_string("0Vdc");
-	}
 }
