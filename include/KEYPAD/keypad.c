3563c369981c72bc1967cfa8158b6c7aa205d963
flo071 <flo.hintermeier@gmail.com>
2018-04-09 21:25:36 +0200

    beautify code

--- include/KEYPAD/keypad.c
+++ include/KEYPAD/keypad.c
@@ -7,70 +7,70 @@ Created by flo071
 #include <util/delay.h>
 #include "keypad.h"
 
-int KEYPAD_setup(){
+int KEYPAD_setup() {
 	DDRD  = REGen;
 	PORTD = PORTen;
 }
 
-int keypad(out){
+int keypad(out) {
 
-	while(1){
+	while (1) {
 		out = 0;
 
-		PORTD=COLUMN1;	//column 1
-		if((PIND | ROW1)==ROW1){
-			out=out+1;		//key 1
+		PORTD = COLUMN1;	//column 1
+		if ((PIND | ROW1) == ROW1) {
+			out = out + 1;		//key 1
 		}
-		if((PIND | ROW2)==ROW2){
-			out=out+4;		//key 4
+		if ((PIND | ROW2) == ROW2) {
+			out = out + 4;		//key 4
 		}
-		if((PIND | ROW3)==ROW3){
-			out=out+7;		//key 7
+		if ((PIND | ROW3) == ROW3) {
+			out = out + 7;		//key 7
 		}
-		if((PIND | ROW4)==ROW4){
-			out=out+10;		//key *
-		}
-
-		PORTD=COLUMN2;	//column 2
-		if((PIND | ROW1)==ROW1){
-			out=out+2;		//key 2
-		}
-		if((PIND | ROW2)==ROW2){
-			out=out+5;		//key 5
-		}
-		if((PIND | ROW3)==ROW3){
-			out=out+8;		//key 8
-		}
-		if((PIND | ROW4)==ROW4){
-			out=out+11;		//key 0
+		if ((PIND | ROW4) == ROW4) {
+			out = out + 10;		//key *
 		}
 
-		PORTD=COLUMN3;	//column 3
-		if((PIND | ROW1)==ROW1){
-			out=out+3;		//key 3
+		PORTD = COLUMN2;	//column 2
+		if ((PIND | ROW1) == ROW1) {
+			out = out + 2;		//key 2
 		}
-		if((PIND | ROW2)==ROW2){
-			out=out+6;		//key 6
+		if ((PIND | ROW2) == ROW2) {
+			out = out + 5;		//key 5
 		}
-		if((PIND | ROW3)==ROW3){
-			out=out+9;		//key 9
+		if ((PIND | ROW3) == ROW3) {
+			out = out + 8;		//key 8
 		}
-		if((PIND | ROW4)==ROW4){
-			out=out+12;		//key #
+		if ((PIND | ROW4) == ROW4) {
+			out = out + 11;		//key 0
 		}
 
-		PORTD=COLUMN4; //column 4
-		if((PIND | ROW1)==ROW1){
-			out=out+13;		//key A
+		PORTD = COLUMN3;	//column 3
+		if ((PIND | ROW1) == ROW1) {
+			out = out + 3;		//key 3
 		}
-		if((PIND | ROW2)==ROW2){
-			out=out+14;		//key B
+		if ((PIND | ROW2) == ROW2) {
+			out = out + 6;		//key 6
 		}
-		if((PIND | ROW3)==ROW3){
-			out=out+15;		//key C
+		if ((PIND | ROW3) == ROW3) {
+			out = out + 9;		//key 9
 		}
-		if((PIND | ROW4)==ROW4){
-			out=out+16;		//key D
+		if ((PIND | ROW4) == ROW4) {
+			out = out + 12;		//key #
+		}
+
+		PORTD = COLUMN4; //column 4
+		if ((PIND | ROW1) == ROW1) {
+			out = out + 13;		//key A
+		}
+		if ((PIND | ROW2) == ROW2) {
+			out = out + 14;		//key B
+		}
+		if ((PIND | ROW3) == ROW3) {
+			out = out + 15;		//key C
+		}
+		if ((PIND | ROW4) == ROW4) {
+			out = out + 16;		//key D
 		}
 	}
 	return out;
--- main.c
+++ main.c
@@ -9,44 +9,44 @@ Created by flo071
 #include "include/KEYPAD/keypad.c"
 #include "include/SIGNAL/signal.c"
 
-int value=0;
+int value = 0;
 
-int setup(void){
+int setup(void) {
 	//Disable JTAG
-    MCUCR =  (1<<JTD);
-    MCUCR =  (1<<JTD);
+	MCUCR =  (1 << JTD);
+	MCUCR =  (1 << JTD);
 }
 
-int main(void){
+int main(void) {
 	setup();
 	SPI_setup();
 	LCD_setup();
 	KEYPAD_setup();
-    while(1){
-    	keypad_display_test();
-    }
+	while (1) {
+		keypad_display_test();
+	}
 }
 
-int keypad_display_test(){
+int keypad_display_test() {
 	keypad();
 	value = keypad();
-	switch(value){
-		case  0: lcd_string(" 0 has been pressed.");
-		case  1: lcd_string("Sin output"); signal_sin();
-		case  2: lcd_string("Tri output"); signal_tri();
-		case  3: lcd_string("Rect output"); signal_rect();
-		case  4: lcd_string(" 4 has been pressed.");
-		case  5: lcd_string(" 5 has been pressed.");
-		case  6: lcd_string(" 6 has been pressed.");
-		case  7: lcd_string(" 7 has been pressed.");
-		case  8: lcd_string(" 8 has been pressed.");
-		case  9: lcd_string(" 9 has been pressed.");
-		case 10: lcd_string(" * has been pressed.");
-		case 11: lcd_string(" 0 has been pressed.");
-		case 12: lcd_string(" # has been pressed.");
-		case 13: lcd_string(" A has been pressed.");
-		case 14: lcd_string(" B has been pressed.");
-		case 15: lcd_string(" C has been pressed.");
-		case 16: lcd_string(" D has been pressed.");
+	switch (value) {
+	case  0: lcd_string(" 0 has been pressed.");
+	case  1: lcd_string("    Sine output     "); signal_sin();
+	case  2: lcd_string("  Triangle output   "); signal_tri();
+	case  3: lcd_string("  Rectangle output  "); signal_rect();
+	case  4: lcd_string(" 4 has been pressed.");
+	case  5: lcd_string(" 5 has been pressed.");
+	case  6: lcd_string(" 6 has been pressed.");
+	case  7: lcd_string(" 7 has been pressed.");
+	case  8: lcd_string(" 8 has been pressed.");
+	case  9: lcd_string(" 9 has been pressed.");
+	case 10: lcd_string(" * has been pressed.");
+	case 11: lcd_string(" 0 has been pressed.");
+	case 12: lcd_string(" # has been pressed.");
+	case 13: lcd_string(" A has been pressed.");
+	case 14: lcd_string(" B has been pressed.");
+	case 15: lcd_string(" C has been pressed.");
+	case 16: lcd_string(" D has been pressed.");
 	}
 }
